2b41b0283e9d7fd2d41f2c3467a51d3f33db2441
Hong Phuc <62658865+zzKent@users.noreply.github.com>
2020-10-05 08:47:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jee", "Jin", "Anie", "David", "Tony", "Pele", "Jack", "Lucas"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
